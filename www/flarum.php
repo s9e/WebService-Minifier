e8c22a10522728b49c4d34c5ab70512c65aa5f15
JoshyPHP <s9e.dev@gmail.com>
2016-01-02 04:24:43 +0100

    Added a front end configured to minify Flarum's assets

---
+++ www/flarum.php
@@ -0,0 +1,14 @@
+<?php
+
+include __DIR__ . '/../vendor/autoload.php';
+
+$minifier = new s9e\WebServices\Minifier\Minifier;
+$minifier->maxPayload = 2000000;
+$minifier->minifiers = [
+	'ClosureCompilerService' => [
+		'compilationLevel' => 'SIMPLE_OPTIMIZATIONS',
+		'timeout'          => 60
+	]
+];
+$minifier->mustContain = null;
+$minifier->handleRequest();
