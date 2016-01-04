2bc5ac18379191bc994dc5e827a223f1e46d571b
JoshyPHP <s9e.dev@gmail.com>
2016-01-04 22:19:20 +0100

    Added tidy script

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Make sure that `www/cache` is writable.
 ### API
 
 `www/minify.php` accepts POST requests, receives the original source code as raw data in the request's body and returns the minified code in the response body.
+
+### Maintenance
+
+There are no limits on the cache dir. You can periodically run `scripts/tidy.php 123` to delete all but the newest `123` MB of data from the cache.
---
+++ scripts/tidy.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php
+
+// Use the first argument as max size or default to ~10 MB
+$maxSize = (isset($_SERVER['argv'][1])) ? $_SERVER['argv'][1] * 1048576 : 10e6;
+
+// Stat all the .gz file in the cache dir
+$files = [];
+foreach (glob(__DIR__ . '/../www/cache/*.gz') as $filepath)
+{
+	$files[$filepath] = stat($filepath);
+}
+
+// Sort by creation time
+array_multisort($files, 'ctime');
+
+// Remove files from the list until we exceed $maxSize
+$size = 0;
+while (!empty($files) && $size < $maxSize)
+{
+	$file  = array_pop($files);
+	$size += $file['size'];
+}
+
+// Delete the remaining files
+array_map('unlink', $files);
