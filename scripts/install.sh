f2c6b387c75324301f073589311f8a7490b35657
JoshyPHP <s9e.dev@gmail.com>
2016-07-28 22:05:04 +0200

    Updated library and install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,9 @@ wget -cq https://getcomposer.org/composer.phar -O /tmp/composer.phar &
 wait
 
 cd "$(dirname $(dirname $0))/bin"
-unzip -n /tmp/compiler-latest.zip compiler.jar
+unzip -nq /tmp/compiler-latest.zip
+rm COPYING README.md
+mv -f closure-compiler-*.jar compiler.jar
 chmod 0444 compiler.jar
 
 cd ..
